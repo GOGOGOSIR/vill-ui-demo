0e9912759ef9e41cda95d28c0437f7917c06def8
eric <>
2020-08-11 15:42:05 +0800

    fix: 更新vill-ui0.4.9,修复icon字体过粗

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 const {resolve} = require('path')
 module.exports = {
-  publicPath: '/vill-ui-demo/',
-  chainWebpack: config => {
-    // 查看打包文件体积大小
-    config
-      .plugin('webpack-bundle-analyzer')
-      .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+  publicPath: '/',
+  // chainWebpack: config => {
+  //   // 查看打包文件体积大小
+  //   config
+  //     .plugin('webpack-bundle-analyzer')
+  //     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+  // },
+  devServer: {
+    host: '0.0.0.0'
   }
 }
